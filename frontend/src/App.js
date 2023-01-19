eeb186520df31e0dfeccd3e48a8b945a56c17c65
Radek Grędel <radoslaw.gredel@student.pk.edu.pl>
2023-01-19 01:09:28 +0100

    adds login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import { useLocalState } from './util/useLocalStorage';
+import Dashboard from './components/Dashboard';
+import Homepage from './components/Homepage';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 
-function App() {
-  const reqBody = {
-    login: "radek",
-    password: "zaq1@WSX",
-  };
+function App() {  
 
-  fetch("/persons/auth/authenticate", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "post",
-    body: JSON.stringify(reqBody),
-  })
-    .then((response) => Promise.all([response.json(), response.headers]))
-    .then(([body]) => console.log(body.token));
-  
   return (
-    <div className="App">
-  
-    </div>
+    <Routes>
+      <Route path="/dashboard" element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>}/>
+      <Route path="/" element={<Homepage />}/>
+      <Route path="/login" element={<Login />}/>
+    </Routes>
   );
 }
 
